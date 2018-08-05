98188e33e0d7e3100c9d4554e7cf54ee47000f3d
Mairaj Khoso <khoso_mairaj@yahoo.com>
2018-08-05 19:31:35 +0500

    6 August Commit 2018

--- fbIntegration/ViewController.swift
+++ fbIntegration/ViewController.swift
@@ -33,23 +33,7 @@ class ViewController: UIViewController {
         
         
        
-        
-            
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-
-        
-        
-        
+   
         
         
         // Do any additional setup after loading the view, typically from a nib.
